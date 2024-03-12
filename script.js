2e1ba4ec0e1737c105826780ab43c5043f170901
weeezik <mkihanya.33@gmail.com>
2024-03-12 19:08:22 +0000

    added html, css, and js file
    linked html and js file. also, copied in code from
    codepen file called "calculator logic draft".

---
+++ script.js
@@ -0,0 +1,30 @@
+//add"click" event listeners for each button
+//when 2 button is clicked, set num1 to 2
+//when + button is clicked, set operator to "+"
+//when 3 button is clicked, set num2 to 3
+//when = button clicked, display console.log(operate(operator, num1, num2))
+//when clear button clicked, remove any/all contents from display (might have to reset num1, operator, and num2 variables to null)
+
+//operate function
+let operator = "-"
+let num1 = 10
+let num2 = 5
+const operate = function (operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+            break;
+        case "-":
+            return num1 - num2;
+            break;
+        case "*":
+            return num1 + num2;
+            break;
+        case "/":
+            return num1 / num2;
+            break;
+    }
+}
+
+console.log("Should be 4: [" + operate("+", 2, 2) + "] ...Yup it's 4.")
+console.log("Via variable operation: " + operate(operator, num1, num2))

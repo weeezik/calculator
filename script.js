b08fb515376beb23b5b3fb7945090966138bfffd
weeezik <mkihanya.33@gmail.com>
2024-03-14 17:35:46 +0000

    Store num1 and operator variables successfully
    halfway through calculation logic. Num1 and operator variables can
    be returned and used in operator function.

--- script.js
+++ script.js
@@ -9,12 +9,14 @@
 const display = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number-button");
 const clearButton = document.querySelector("#clear");
+const operateButtons = document.querySelectorAll(".operate-button");
+
 
 
 //use the forEach method to loop through each element and add an event listener
 
-numberButtons.forEach(function(button) {
-    button.addEventListener("click", function() {
+numberButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
         let returnValue = display.textContent;
         let displayValue = returnValue;
         let buttonValue = (event.target.textContent).toString();
@@ -27,13 +29,11 @@ clearButton.addEventListener("click", function () {
     display.textContent = " ";
 });
 
-
-
-
 //operate function
-let operator = "-"
-let num1 = 10
-let num2 = 5
+//hypothetical values
+// let operator = "-"
+// let num1 = 10
+// let num2 = 5
 const operate = function (operator, num1, num2) {
     switch (operator) {
         case "+":
@@ -51,5 +51,34 @@ const operate = function (operator, num1, num2) {
     }
 }
 
-console.log("Should be 4: [" + operate("+", 2, 2) + "] ...Yup it's 4.")
-console.log("Via variable operation: " + operate(operator, num1, num2))
+console.log("Operate function check --- Should be 4: [" + operate("+", 2, 2) + "] ...Yup it's 4.")
+// console.log("Via variable operation: " + operate(operator, num1, num2))
+
+operateButtons.forEach(function (theOperators) {
+    theOperators.addEventListener("click", function () {
+        let num1 = display.textContent;
+        switch (event.target.textContent) {
+            case "+":
+                operator = "+";
+            case "-":
+                operator = "-";
+            case "x":
+                operator = "*";
+            case "÷":
+                operator = "/"
+        }
+        let firstStep = [num1, operator];
+        console.log(firstStep)
+        return firstStep;
+    })
+});
+
+//only after operator is selected: when a number button is pressed the 
+//display is cleared and new num2 can be entered
+
+//when equals button is pressed, num2=display.textContent 
+//and then run the operate function
+
+//equal button logic in WHITE NOTEBOOK
+//update display.textContent to 
+// display.textContent = operate(operator, num1, num2);

0c38208464075ebd86b7b7e80ca2cd00cbc5fc89
weeezik <mkihanya.33@gmail.com>
2024-03-15 16:58:05 +0000

    Equations array stores num1 and num2 at...
    Their respective indices. Let's go!

--- script.js
+++ script.js
@@ -1,51 +1,8 @@
-//add"click" event listeners for each button
-//when 2 button is clicked, set num1 to 2
-//when + button is clicked, set operator to "+"
-//when 3 button is clicked, set num2 to 3
-//when = button clicked, display console.log(operate(operator, num1, num2))
-//when clear button clicked, remove any/all contents from display (might have to reset num1, operator, and num2 variables to null)
-
-const display = document.querySelector(".display");
-const numberButtons = document.querySelectorAll(".number-button");
-const clearButton = document.querySelector("#clear");
-const operateButtons = document.querySelectorAll(".operate-button");
-const equalsButton = document.querySelector("#equal")
-
-
-//use the forEach method to loop through each element and add an event listener
-numberButtons.forEach(function (theNumbers) {
-    theNumbers.addEventListener("click", function () {
-        let returnValue = display.textContent;
-        let displayValue = returnValue;
-        let buttonValue = (event.target.textContent).toString();
-        returnValue = displayValue += buttonValue;
-        display.textContent = returnValue;
-    })
-});
-//operator (content of button clicked) should be added to the display, similar to numberButtons function
-operateButtons.forEach(function (theOperators) {
-    theOperators.addEventListener("click", function () {
-        let displayValue = display.textContent
-        let operator = event.target.textContent;
-        display.textContent = displayValue.concat(operator);
-    })
-})
-
-clearButton.addEventListener("click", function () {
-    display.textContent = " ";
-});
-
-equalsButton.addEventListener("click", function () {
-    //sift variables
-    //run operate function
-})
-
-
 //operate function
-    //hypothetical values
-    // let operator = "-"
-    // let num1 = 10
-    // let num2 = 5
+//hypothetical values
+// let operator = "-"
+// let num1 = 10
+// let num2 = 5
 const operate = function (operator, num1, num2) {
     switch (operator) {
         case "+":
@@ -62,6 +19,63 @@ const operate = function (operator, num1, num2) {
             break;
     }
 }
+
+//add"click" event listeners for each button
+//when 2 button is clicked, set num1 to 2
+//when + button is clicked, set operator to "+"
+//when 3 button is clicked, set num2 to 3
+//when = button clicked, display console.log(operate(operator, num1, num2))
+//when clear button clicked, remove any/all contents from display (might have to reset num1, operator, and num2 variables to null)
+
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number-button");
+const clearButton = document.querySelector("#clear");
+const operateButtons = document.querySelectorAll(".operate-button");
+const equalsButton = document.querySelector("#equal")
+
+let operator;
+let num1;
+let num2;
+
+//use the forEach method to loop through each element and add an event listener
+numberButtons.forEach(function (theNumbers) {
+    theNumbers.addEventListener("click", function () {
+        let returnValue = display.textContent;
+        let displayValue = returnValue;
+        let buttonValue = (event.target.textContent).toString();
+        returnValue = displayValue += buttonValue;
+        display.textContent = returnValue;
+    })
+});
+
+
+//operator (content of button clicked) should be added to the display, similar to numberButtons function
+operateButtons.forEach(function (theOperators) {
+    theOperators.addEventListener("click", function () {
+        let displayValue = display.textContent
+        operator = event.target.textContent;
+        console.log(operator);
+        display.textContent = displayValue.concat(operator);
+        return operator
+    })
+});
+
+
+clearButton.addEventListener("click", function () {
+    display.textContent = " ";
+});
+
+equalsButton.addEventListener("click", function () {
+    let currentDisplay = display.textContent;   
+    const equationArray = currentDisplay.split(operator);
+    console.log(equationArray);
+    let num1 = equationArray[0];
+    let num2 = equationArray[1];
+    currentDisplay = operate(operator, num1, num2);
+})
+
+
+
 
 console.log("Operate function check --- Should be 4: [" + operate("+", 2, 2) + "] ...Yup it's 4.")
 // console.log("Via variable operation: " + operate(operator, num1, num2))
